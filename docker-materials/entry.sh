903e7aeb16f8b7a9f059eb4122acb06ef16c45dd
Jason Williams <JasonJWilliamsNY@gmail.com>
2022-11-08 08:14:21 -0500

    update bash profile

--- docker-materials/entry.sh
+++ docker-materials/entry.sh
@@ -1,6 +1,30 @@
+#!/usr/bin/env bash
 
-#!/bin/bash
+set -e
 
-echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
+# This script is needed to get the permissions correct when copying files from a file system that is mounted in
+# read-only mode by the iRODS CSI Driver. Simply doing a resursive copy causes the directory permissions to become
+# corrupt, which prevents any access to the copied directories at all, for example:
+#
+# d????????? ? ? ? ?            ? data
+# d????????? ? ? ? ?            ? notebooks
+#
+# I haven't been able to find a way to fix the problem after the copies have been made, and this only happens when
+# the file system is mounted in read-only mode.
 
-exec jupyter lab --no-browser --allow-root --ip=0.0.0.0
+# Create the directory structure.
+for d in $(find data/input/* -type d); do
+    mkdir -p "work/${d#*/*/}"
+done
+
+# Copy the files.
+for f in $(find data/input/* -type f); do
+    cp $f "work/${f#*/*/}"
+done
+
+# Fix the permissions on the files.
+find work -type f -exec chmod 0644 {} +
+
+
+
+exec jupyter-lab --no-browser --ip=0.0.0.0 --LabApp.token="" --LabApp.password=""
